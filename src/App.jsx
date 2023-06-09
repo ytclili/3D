99b3434e2228dc47f397626d4f6639b77f313c70
sunyifei <sunyifei@paraview.cn>
2023-06-09 08:50:18 +0800

    threejs WebGpu渲染有问题

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Olympic from './pages/Olympic/index';
 import Island from './pages/Island/index';
 import Explorer from './pages/Explorer';
 import Virtual from './pages/virtual';
+import WebGLVirtual from './pages/virtual - 副本';
 function App() {
     return (
         <Routes>
@@ -18,10 +19,14 @@ function App() {
                 path="/island"
                 element={<Island />}
             />
-            <Route
+             <Route
+                path="/"
+                element={<WebGLVirtual />}
+            />
+            {/* <Route
                 path="/"
                 element={<Virtual />}
-            />
+            /> */}
         </Routes>
     );
 }
