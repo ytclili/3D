bb348a83e302ec18e84dcf265ca8155e0b564f47
sunyifei <634692580@qq.com>
2024-05-07 21:10:41 +0800

    chore: test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 # deploy
-echo Deploy Project
+echo "Deploy Project"
 
+echo "Pulling latest changes from Git..."
 git pull
 
+echo "Removing all Docker containers and images..."
 docker system prune --all --force
 
+echo "Building Docker image my-page-3d..."
 docker build --no-cache -t my-page-3d .
+
+echo "Running Docker container my-page-3d..."
 docker run -d -p 3000:80 my-page-3d
