76a7a8da970b378e98020a65966d31b2d0302d91
sunyifei <634692580@qq.com>
2024-05-08 20:45:03 +0800

    chore: test

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,13 @@ git pull
 wait # 等待上一个命令完成
 echo "pulling is complete ==========>>>>>>>>>>"
 echo "Removing all Docker containers and images..."
+
+docker stop my-page-3d
+
 docker system prune --all --force
 
 echo "Building Docker image my-page-3d..."
 docker build --no-cache -t my-page-3d .
 
 echo "Running Docker container my-page-3d..."
-docker run -d -p 3000:80 my-page-3d
+docker run -d -p 3000:80 --name my-page-3d my-page-3d
